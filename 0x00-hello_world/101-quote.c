e9ec54cb8862a63c8216ef32f5a7e44e93306ecf
Ciira john <njaujohn002@gmail.com>
2022-04-07 17:57:07 +0000

    Advance Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: return 1  after program execution
-*/
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-FILE *fp;
-char str[] = "\" and that piece of art is useful\" - Dora Korpar,2015-10-19";
-fp = fopen("file.txt","w");
-fwrite(str,1,sizeof(str),fp);
-fclose(fp);
-return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
