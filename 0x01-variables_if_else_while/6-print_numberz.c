efb69ae5f8abd74c7572d34937ca2ada9867e606
Ciira john <njaujohn002@gmail.com>
2022-04-08 12:55:22 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+
+{
+int 1;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+
+return (0);
+
+}
