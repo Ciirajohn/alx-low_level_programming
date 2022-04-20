4b9f12d6568808c2510eae2bbe40bb72564dc881
Ciira john <njaujohn002@gmail.com>
2022-04-20 17:20:22 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+
+i = 0;
+for (i = 0; s[i] != '\0'; ++i);
+
+printf("Length of the string: %d", i);
+
+return 0;
