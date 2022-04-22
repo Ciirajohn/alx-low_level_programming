bd5ed00d4bea53d48c1ab8b936258466cb731549
Ciira john <njaujohn002@gmail.com>
2022-04-22 10:48:39 +0000

    My commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+int h, i;
+
+
+h =0;
+while (dest[h] != '\0')
+{
+h++;
+}
+
+i = 0;
+while (src[i] != '\0')
+{
+dest[h] = src[i];
+i++;
+h++;
+}
+dest[h] = '\0';
+
+return (dest);
+
+}
