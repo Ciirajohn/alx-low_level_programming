9012ecd1abf7e81c7d64a1fa6225c0792d1eae11
Ciirajohn <njaujohn002@gmail.com>
2022-04-24 09:56:21 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,6 @@
  *@dest: A pointer to a character that will be changed
  *@src: A pointer to a character that will also be changed
  *Return: dest
- */#include "main.h"
-/**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will also be changed
- *Return: dest
  */
 
 char *_strcat(char *dest, const char *src)
