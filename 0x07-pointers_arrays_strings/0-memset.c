cbc733276c5498d4d806ee277c0e13d3441fbaeb
Ciirajohn <njaujohn002@gmail.com>
2022-04-26 07:42:11 +0000

    My commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*_memset - fills memory with constant byte
+*
+*@s: pointer block of memory to fill
+*@b: value of set
+*@n: bytes of memory
+*Return: dest
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+    unsigned int i;
+
+    /*Declaring FOR*/
+
+    for (i = 0; i < n; i++)
+
+     {
+         *(s + i) = b;
+
+     }
+
+      return (s);
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Alx Low Level Programming
+My hello world for 0x00-hello_world
+My 0x07-pointers_arrays_strings readme
