83252525555c268ef306abd7d7ed82cd60f6b789
Ciirajohn <njaujohn002@gmail.com>
2022-04-26 09:22:11 +0000

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,17 +11,16 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
 
-    unsigned int i;
+ unsigned int i;
 
-    /*Declaring FOR*/
+ /*Declaring FOR*/
 
-    for (i = 0; i < n; i++)
+ for (i = 0; i < n; i++)
 
-     {
-         *(s + i) = b;
+ {
+ *(s + i) = b;
+ }
 
-     }
-
-      return (s);
+ return (s);
 
 }
