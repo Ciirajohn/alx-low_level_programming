7c5cf985edd0c3859f22e01c3b17ff12b7d772c3
Ciirajohn <njaujohn002@gmail.com>
2022-04-26 09:00:40 +0000

    Task 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+*_strspn -  string to be  searched
+*@s: string
+*@accept: This is the string containing the list of characters
+*Return: return count
+*/
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+
+   int i, j;
+
+   int count = 0;
+
+   for (i = 0; s[i] != '\0'; i++)
+
+   {
+       if (s[i] != 32)
+
+       {
+            for(j = 0; accept[j] != '\0'; j++)
+
+
+             {
+                    if (s[i] == accept[j])
+                    count++;
+             }
+       }
+
+        else
+            return (count);
+
+   }
+
+     return (count);
+}
