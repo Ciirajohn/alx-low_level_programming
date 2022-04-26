b40bd2fbdc89585483e3ebf925fc6167f20d9faa
Ciirajohn <njaujohn002@gmail.com>
2022-04-26 09:54:50 +0000

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,35 +7,23 @@
 */
 
 unsigned int _strspn(char *s, char *accept)
-
 {
+	int i, j;
+	int count = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++;
+			}
+		}
+		else
+			return (count);
 
-int i, j;
-
-int count = 0;
-
-for (i = 0; s[i] != '\0'; i++)
-
-{
-
-if (s[i] != 32)
-
-for(j = 0; accept[j] != '\0'; j++)
-
-
-{
-if (s[i] == accept[j])
-count++;
-}
-
-}
-
-else
-
-return (count);
-
-}
-
-return (count);
+	}
+	return (count);
 }
