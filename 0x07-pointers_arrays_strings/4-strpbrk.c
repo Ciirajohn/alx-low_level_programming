46d0c10a6f1b04505a883250c5ca473dfe0e3c36
Ciirajohn <njaujohn002@gmail.com>
2022-04-26 10:29:17 +0000

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*_strpbrk - locates the first occurrence in the string
+*@s: string to be scanned 
+*@accept:character in str1 that matches one of the characters in str2
+*Return: strings that matches any character specified in accept
+*/
+
+char *_strpbrk(char *s, char *accept)
+
+{
+
+   int j;
+
+   while (*s != '\0')
+   {
+           j =0;
+           while (accept[j] != '\0')
+           {
+                   if (*s == accept[j])
+                   {
+                           return (s);
+                   }
+
+                   j++;
+            }
+
+            s++;
+    }
+    return (0);
+
+}
