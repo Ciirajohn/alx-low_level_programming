f6a85aeea4523eb141eff51507294d4c27f62e40
Ciirajohn <njaujohn002@gmail.com>
2022-04-28 10:35:24 +0000

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
  *Return: pow recursion
  */
 
-int _pow_recursion(int x , int y)
+int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
@@ -14,7 +14,7 @@ return (-1);
 }
 else if (y != 0)
 
-return (_pow_recursion(x , y - 1) * x);
+return (_pow_recursion(x, y - 1) * x);
 
 else
 return (1);
