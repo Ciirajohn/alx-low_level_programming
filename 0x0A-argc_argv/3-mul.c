954cbe0b84fc4b76e29784384e56ec29e7c85ad1
Ciirajohn <njaujohn002@gmail.com>
2022-05-05 10:20:23 +0000

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,15 @@ int main(int argc, char *argv[])
 
 {
 
-int a, b, p;
-
-printf("Enter two numbers: ");
-
-scanf("%lf %lf", &a, &b);
-
-p = a * b;
-
-printf("p = %.2lf", p);
-
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
 return (0);
+}
+printf("Error\n");
+return (1);
 
 }
