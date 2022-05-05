1d3d2007769942590f4ca2e670593c56c7e9411a
Ciirajohn <njaujohn002@gmail.com>
2022-05-05 09:55:49 +0000

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 
 /**
 * main - Print the name of the program
-* @argc: Count arguments 
+* @argc: Count arguments
 * @argv: Arguments
 *
 * Return: Always 0 (success)
@@ -14,9 +14,11 @@ int main(int argc, char *argv[])
 {
 int counter;
 
-for(counter =0; counter < agrc; counter++)
+for (counter =0; counter < agrc; counter++)
 {
-printf("argv[%2d]: %s\n", counter, argv[counter]):
+
+printf("argv[%2d]: %s\n", counter, argv[counter]);
+
 }
 
 return (0);
